e5fa59fab374c509b58c632af94d961795bedb89
nethereal <jcourtes@gmail.com>
2018-12-12 22:40:38 -0500

    1.2 done

---
+++ 1.2.ps1
@@ -0,0 +1,13 @@
+﻿$inputData = Get-Content C:\gitstuff\AdventOfCode2018-PS\input-1.2.txt
+$ncnt = 0
+$ubound = $inputData.Count
+$current = 0
+$history = @(0)
+do {
+    If ($ncnt -eq $ubound) { $ncnt = 0 }
+    $current = $current + [int]$inputData[$ncnt]
+    if ($history -contains $current) { break }
+    $history = $history + $current
+    $ncnt = $ncnt + 1
+} while ($true)
+Write-Host "$current repeated!"
